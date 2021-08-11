b50f2ec3470e312332d7ab8e82e03fde647ed2d0
Supun Viraj <supunr@99x.lk>
2021-08-11 23:53:47 +0530

    Basic DRF Configurations

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
 
-# Create your views here.
+from main.models import User
+from main.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
